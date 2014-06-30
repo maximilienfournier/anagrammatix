74a9439490830a24e6809c54de083bdcafc70d2f
maximilienfournier <maximilien.fournier89@gmail.com>
2014-06-30 15:29:59 +0200

    Enhanced openQuestionScoring
    - Calculates the proximity with the correct answer in
    openQuestionScoring, instead of just comparing it. (for instance PaRi
    would be a valid if the correct answer if Paris)

--- public/app.js
+++ public/app.js
@@ -16,6 +16,7 @@ jQuery(function($){
         init: function() {
             IO.socket = io.connect();
             IO.bindEvents();
+
         },
 
         /**
@@ -220,6 +221,7 @@ jQuery(function($){
 
             // Initialize the fastclick library
             FastClick.attach(document.body);
+
 
         },
 
@@ -1284,7 +1286,17 @@ jQuery(function($){
                 console.log('playerAnswer '+playerAnswer);
                 
                 if (typeof(playerAnswer) != 'undefined'){
+                    /*
                     if (playerAnswer === App.Host.questionData.arrayOfAnswers[0]['value']){
+                        scoreForThisRound = App.Host.maxPoints;
+                    }
+                    else{
+                        scoreForThisRound = App.Host.minPoints;
+                    }
+                    */
+                    // The Levenshtein represents the distance between the two strings
+                    var levenshtein = App.getEditDistance(playerAnswer.toLowerCase(), App.Host.questionData.arrayOfAnswers[0]['value'].toLowerCase());
+                    if (levenshtein < 4){
                         scoreForThisRound = App.Host.maxPoints;
                     }
                     else{
@@ -1973,6 +1985,40 @@ jQuery(function($){
                     maxFontSize:300
                 }
             );
+        },
+
+        getEditDistance : function(a, b) {
+          if(a.length === 0) return b.length; 
+          if(b.length === 0) return a.length; 
+         
+          var matrix = [];
+         
+          // increment along the first column of each row
+          var i;
+          for(i = 0; i <= b.length; i++){
+            matrix[i] = [i];
+          }
+         
+          // increment each column in the first row
+          var j;
+          for(j = 0; j <= a.length; j++){
+            matrix[0][j] = j;
+          }
+         
+          // Fill in the rest of the matrix
+          for(i = 1; i <= b.length; i++){
+            for(j = 1; j <= a.length; j++){
+              if(b.charAt(i-1) == a.charAt(j-1)){
+                matrix[i][j] = matrix[i-1][j-1];
+              } else {
+                matrix[i][j] = Math.min(matrix[i-1][j-1] + 1, // substitution
+                                        Math.min(matrix[i][j-1] + 1, // insertion
+                                                 matrix[i-1][j] + 1)); // deletion
+              }
+            }
+          }
+         
+          return matrix[b.length][a.length];
         },
 
         draw : function () {
